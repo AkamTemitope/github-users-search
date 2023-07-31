d24123532ae2e0b112ef5101088d1afd6bfa05f5
Akamtemitope <temitopeakam@gmail.com>
2023-07-31 18:36:28 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        babyPowder: "#FBFEFB",
+        buff: "#CF995F",
+      },
+    },
+  },
+  plugins: [],
+};
